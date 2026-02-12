e2092d003a02e6cf40716aae760e50860dd5b55b
Shubham11211 <shubham.s@labra.io>
2026-02-12 15:48:02 +0530

    this is error solve

--- app.py
+++ app.py
@@ -60,7 +60,7 @@ def home():
         "Home endpoint hit",
         extra={
             "http.method": "GET",
-            "http.path": "/" 
+            "http.path": "/",
             "error.type": "CustomError"
         }
     )
